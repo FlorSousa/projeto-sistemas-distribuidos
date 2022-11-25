a049731e60464d88d3650a9f72b82f60df725de3
Erick-Fernandes-dev <erick.farias@dcx.ufpb.br>
2022-11-25 13:24:17 -0300

    Falta testar

--- servidor/index/sequelize.js
+++ servidor/index/sequelize.js
@@ -1,5 +1,8 @@
 import Sequelize from "sequelize";
-const sequelize = new Sequelize(process.env.POSTGRES_DB,process.env.POSTGRES_USER, process.env.POSTGRES_DB,{
+import dotenv from 'dotenv'
+dotenv.config()
+console.log(process.env.POSTGRES_PASSWORD)
+const sequelize = new Sequelize(process.env.POSTGRES_DB,process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD,{
     host:process.env.POSTGRES_HOST,
     port:'5432',
     dialect:'postgres'
