accaacbc5033a8c8900750d86a14abad6c3af544
jhonatas <jhonatas.flor@dcx.ufpb.br>
2022-11-27 06:38:56 -0300

    bck

--- servidor/index/sequelize.js
+++ servidor/index/sequelize.js
@@ -1,7 +1,7 @@
 import Sequelize from "sequelize";
 import dotenv from 'dotenv'
 dotenv.config()
-console.log(process.env.POSTGRES_PASSWORD)
+console.log(process.env.POSTGRES_DB)
 const sequelize = new Sequelize(process.env.POSTGRES_DB,process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD,{
     host:process.env.POSTGRES_HOST,
     port:'5432',
